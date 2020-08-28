f18bb0a614cfb61eb54a3e426b3c5d75242a4534
B L Λ C K <ixd.abhi@gmail.com>
2020-08-28 01:10:52 -0400

    vue cli project

---
+++ website/src/firebase/fire.js
@@ -0,0 +1,38 @@
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBUEGfpdaQp4PWIn-jdP_JEf9JwafzZtaw",
+  authDomain: "gonusabooking.firebaseapp.com",
+  databaseURL: "https://gonusabooking.firebaseio.com",
+  projectId: "gonusabooking",
+  storageBucket: "gonusabooking.appspot.com",
+  messagingSenderId: "258941994308",
+  appId: "1:258941994308:web:9787786b2a0dc08c310311",
+  measurementId: "G-NP7121XDBQ"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+// utils
+const auth = firebase.auth()
+const firedb = firebase.firestore()
+const realdb = firebase.database()
+
+// collection references
+const usersCollection = firedb.collection('users')
+const postsCollection = firedb.collection('posts')
+const commentsCollection = firedb.collection('comments')
+const likesCollection = firedb.collection('likes')
+
+// export utils/refs
+export {
+  auth,
+  firedb,
+  realdb,
+  usersCollection,
+  postsCollection,
+  commentsCollection,
+  likesCollection
+}
